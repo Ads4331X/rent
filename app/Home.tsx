086d0c49b8d77e7c65dfd6792417d0afe272db41
Ads4331x <karkitan@gmail.com>
2026-05-16 18:38:38 +0545

    completed ui of login

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,3 +1,5 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React, { useState } from "react";
@@ -9,26 +11,44 @@ export default function Home() {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
+  // Reusable styles
+  const inputContainer =
+    "flex-row items-center rounded-2xl border border-gray-300 bg-gray-100 px-4";
+
+  const inputStyle = "flex-1 py-4 pl-3 text-base text-black";
+
+  const labelStyle = "mb-2 text-gray-600";
+
+  const linkStyle = "font-medium text-blue-500";
+
+  const socialButton = "rounded-full bg-gray-100 p-3";
+
   return (
-    <SafeAreaView className="flex-1 justify-start bg-slate-100 px-6 py-2">
-      <View className="rounded-3xl bg-white px-5 py-8 shadow-lg">
-        {/* Title */}
-        <Text className="text-center text-4xl font-extrabold text-rose-700">
-          Rent Tracker
+    <SafeAreaView className="flex-1 bg-slate-100 px-6 py-2">
+      {/* Title */}
+      <Text className="text-center text-4xl font-extrabold uppercase text-rose-700">
+        Rent Tracker
+      </Text>
+
+      {/* Subtitle */}
+      <View className="mb-5 p-2">
+        <Text className="mt-8 text-center text-2xl font-bold text-gray-700">
+          Welcome Back
         </Text>
 
-        {/* Subtitle */}
-        <Text className="mt-8 px-1 text-lg font-medium text-gray-700">
-          Log in to Your Account
+        <Text className="text-center text-gray-500">
+          Manage your property and track rents with ease.
         </Text>
+      </View>
 
-        {/* Form */}
+      {/* Card */}
+      <View className="rounded-3xl bg-white p-5 pt-2 shadow-lg">
         <View className="mt-6 gap-5">
           {/* Email */}
-          <View className="w-full">
-            <Text className="mb-2 text-gray-600">Email</Text>
+          <View>
+            <Text className={labelStyle}>Email Address</Text>
 
-            <View className="flex-row items-center rounded-2xl border border-gray-300 bg-gray-100 px-4">
+            <View className={inputContainer}>
               <MaterialIcons name="email" size={22} color="#6B7280" />
 
               <TextInput
@@ -39,16 +59,16 @@ export default function Home() {
                 keyboardType="email-address"
                 autoCapitalize="none"
                 autoCorrect={false}
-                className="flex-1 py-4 pl-3 text-base text-black"
+                className={inputStyle}
               />
             </View>
           </View>
 
           {/* Password */}
-          <View className="w-full">
-            <Text className="mb-2 text-gray-600">Password</Text>
+          <View>
+            <Text className={labelStyle}>Password</Text>
 
-            <View className="flex-row items-center rounded-2xl border border-gray-300 bg-gray-100 px-4">
+            <View className={inputContainer}>
               <MaterialIcons name="lock" size={22} color="#6B7280" />
 
               <TextInput
@@ -58,7 +78,7 @@ export default function Home() {
                 onChangeText={setPassword}
                 secureTextEntry={!showPassword}
                 autoCorrect={false}
-                className="flex-1 py-4 pl-3 text-base text-black"
+                className={inputStyle}
               />
 
               <Pressable onPress={() => setShowPassword(!showPassword)}>
@@ -73,7 +93,7 @@ export default function Home() {
 
           {/* Forgot Password */}
           <Pressable className="self-end">
-            <Text className="font-medium text-blue-500">Forgot Password?</Text>
+            <Text className={linkStyle}>Forgot Password?</Text>
           </Pressable>
 
           {/* Login Button */}
@@ -83,6 +103,39 @@ export default function Home() {
             </Text>
           </Pressable>
         </View>
+
+        {/* Divider */}
+        <View className="my-7 flex-row items-center">
+          <View className="h-[1px] flex-1 bg-gray-300" />
+
+          <Text className="mx-4 text-sm text-gray-500">Or Continue With</Text>
+
+          <View className="h-[1px] flex-1 bg-gray-300" />
+        </View>
+
+        {/* Socials */}
+        <View className="flex-row justify-center gap-5">
+          <Pressable className={socialButton}>
+            <AntDesign name="google" size={24} color="black" />
+          </Pressable>
+
+          <Pressable className={socialButton}>
+            <Entypo name="facebook" size={24} color="black" />
+          </Pressable>
+
+          <Pressable className={socialButton}>
+            <AntDesign name="github" size={24} color="black" />
+          </Pressable>
+        </View>
+      </View>
+
+      {/* Bottom */}
+      <View className="flex-1 flex-row items-center justify-center">
+        <Text>Don&#39;t have an account? </Text>
+
+        <Pressable>
+          <Text className={linkStyle}>Sign up</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
